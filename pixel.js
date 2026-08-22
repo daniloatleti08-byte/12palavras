dde0e7e807318e00823cdfb32fcf0be84487dd35
Danilo a Silva <daniloatleti08@gmail.com>
2026-08-22 13:27:36 -0300

    Update pixel.js

--- pixel.js
+++ pixel.js
@@ -4,10 +4,10 @@
   2) Este site dispara PageView, ViewContent e o evento personalizado CheckoutClick.
   3) Como o Pixel já está instalado na Cakto, deixe InitiateCheckout e Purchase para a Cakto.
 */
-const META_PIXEL_ID = 'COLE_SEU_PIXEL_ID_AQUI';
+const META_PIXEL_ID = '1684026826038026';
 
 (function(){
-  if (!META_PIXEL_ID || META_PIXEL_ID === 'COLE_SEU_PIXEL_ID_AQUI') {
+  if (!META_PIXEL_ID || META_PIXEL_ID === '1684026826038026') {
     console.info('[Meta Pixel] Configure o ID em pixel.js');
     return;
   }
